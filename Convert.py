c970089b1fa107b5ed6b70a47b2fbd549c75ed64
Sxyaz <138993668+Sxyaz@users.noreply.github.com>
2023-07-08 18:10:57 +0300

    Update Convert.py

--- Convert.py
+++ Convert.py
@@ -4,7 +4,7 @@ image1 = Image.open("first.jpg") #The first image must be RGBA, and this image u
 image2 = Image.open("second.png") #The second image too,must be RGBA, and the scale must be same with first image.
 image3 = Image.open("third.png")
 #image4 = Image.open("fourth.png")
-image1.save("Result.gif", save_all = True, append_images=[image2, image3, image4], duration = 200, loop = 0)
+image1.save("Result.gif", save_all = True, append_images=[image2, image3], duration = 200, loop = 0)
 
 #You can add more "image" variables, and then just put it in here "append_images=[]".
 #append_images=[image2, image3, image4, image5] like that.
